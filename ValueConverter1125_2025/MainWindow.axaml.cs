0abb1aa1230e3890fda3d831f6f1fc40d87d03b2
stenly87 <stenly87@bk.ru>
2025-12-16 11:55:17 +1000

    Init

---
+++ ValueConverter1125_2025/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace ValueConverter1125_2025;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
